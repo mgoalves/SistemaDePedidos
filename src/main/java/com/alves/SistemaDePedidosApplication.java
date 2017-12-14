ec683b89c0b93e19368ab2a570bd6cf541b4bb2f
Marcello <marcello_doalves@hotmail.com>
2017-12-14 08:28:30 -0200

    Criando projeto.

---
+++ src/main/java/com/alves/SistemaDePedidosApplication.java
@@ -0,0 +1,12 @@
+package com.alves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alves/SistemaDePedidosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alves;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaDePedidosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
