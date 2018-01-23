ddaeaa49482b6279ffcd7e337f651ad67ec5f6a4
Marcello Alves <marcello_doalves@hotmail.com>
2018-01-22 22:18:19 -0200

    Configurando Spring Security

---
+++ src/main/java/com/alves/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.alves.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// Array que permite o usuário resgatar dados mesmo não estando logado. GET.
+	private static final String[] PUBLIC_MATCHERS_GET = { 
+			"/produtos/**", 
+			"/categorias/**" 
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.anyRequest().authenticated();
+		
+		http.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS); // Mantem sem estado
+		
+		http.cors().and().csrf().disable(); // Para retirar defesa contra ataque de sessão
+	}
+
+}
