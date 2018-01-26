784d18874f28405729c46615fd9e933609961b9f
Marcello Alves <marcello_doalves@hotmail.com>
2018-01-26 10:22:37 -0200

    Tratando exceções

--- src/main/java/com/alves/exception/handler/ExceptionHandler.java
+++ src/main/java/com/alves/exception/handler/ExceptionHandler.java
@@ -14,7 +14,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.alves.exception.FileException;
 import com.alves.exception.ObjectNotFoundException;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
@@ -80,5 +84,51 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 				System.currentTimeMillis());
 
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+	}
+
+	// Trata exceções quando existir erros na conversão de arquivo
+	// --------------------------------
+	@org.springframework.web.bind.annotation.ExceptionHandler({ FileException.class })
+	public ResponseEntity<Object> fileException(FileException e, WebRequest request) {
+
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	// Trata exceções AMAZON - 1.
+	// --------------------------------
+	@org.springframework.web.bind.annotation.ExceptionHandler({ AmazonServiceException.class })
+	public ResponseEntity<Object> amazonServiceException(AmazonServiceException e, WebRequest request) {
+
+		HttpStatus statusCode = HttpStatus.valueOf(e.getErrorCode());
+		
+		StandardError error = new StandardError(statusCode.value(), e.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(statusCode).body(error);
+	}
+
+	// Trata exceções AMAZON - 2.
+	// --------------------------------
+	@org.springframework.web.bind.annotation.ExceptionHandler({ AmazonClientException.class })
+	public ResponseEntity<Object> amazonClientException(AmazonClientException e, WebRequest request) {
+
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	// Trata exceções AMAZON - 3.
+	// --------------------------------
+	@org.springframework.web.bind.annotation.ExceptionHandler({ AmazonS3Exception.class })
+	public ResponseEntity<Object> amazonS3Exception(AmazonS3Exception e, WebRequest request) {
+
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/alves/service/S3Service.java
+++ src/main/java/com/alves/service/S3Service.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.alves.exception.FileException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -35,7 +36,7 @@ public class S3Service {
 
 		} catch (IOException e) {
 			
-			throw new RuntimeException("Erro ao converter arquivo para InputStream");
+			throw new FileException("Erro ao converter arquivo para InputStream " + e.getMessage());
 		}
 
 	}
@@ -53,7 +54,7 @@ public class S3Service {
 
 		} catch (URISyntaxException e) {
 			
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI + " + e.getMessage());
 		} 
 	}
 
