cd615544d6a428d6590a7d888bd9d65920797184
Marcello Alves <marcello_doalves@hotmail.com>
2018-01-29 08:37:26 -0200

    Expondo header location

--- src/main/java/com/alves/exception/handler/ExceptionHandler.java
+++ src/main/java/com/alves/exception/handler/ExceptionHandler.java
@@ -3,6 +3,7 @@ package com.alves.exception.handler;
 import javax.management.InvalidAttributeValueException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.alves.exception.FileException;
@@ -43,9 +45,10 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
 	// Trata parametros com dados inválidos
 	// ------------------------------------------------------------
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
-
-		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação", e.getMessage(), request.getRequestURI());
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest request){
+		
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação", e.getMessage(), " ");
 
 		for (FieldError erro : e.getBindingResult().getFieldErrors()) {
 
@@ -58,7 +61,7 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	// Trata exceções quando entidades vinculadas não foram encontradas
 	// --------------------------------
 	@org.springframework.web.bind.annotation.ExceptionHandler({ InvalidAttributeValueException.class })
-	public ResponseEntity<Object> InvalidAttributeValueException(InvalidAttributeValueException e, HttpServletRequest request) {
+	public ResponseEntity<Object> InvalidAttributeValueException(InvalidAttributeValueException e, HttpServletRequest request, WebRequest res) {
 
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Dados inválidos", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
@@ -67,7 +70,7 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	// Trata exceções quando existe permissão de acesso negada.
 	// --------------------------------
 	@org.springframework.web.bind.annotation.ExceptionHandler({ AuthorizationServiceException.class })
-	public ResponseEntity<Object> AuthorizationServiceException(AuthorizationServiceException e, HttpServletRequest request) {
+	public ResponseEntity<Object> AuthorizationServiceException(AuthorizationServiceException e, HttpServletRequest request, WebRequest res) {
 
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Acesso negado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
@@ -76,7 +79,7 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	// Trata exceções quando existir erros na conversão de arquivo
 	// --------------------------------
 	@org.springframework.web.bind.annotation.ExceptionHandler({ FileException.class })
-	public ResponseEntity<Object> fileException(FileException e, HttpServletRequest request) {
+	public ResponseEntity<Object> fileException(FileException e, HttpServletRequest request, WebRequest res) {
 
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro de arquivo", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
@@ -85,7 +88,7 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	// Trata exceções AMAZON - 1.
 	// --------------------------------
 	@org.springframework.web.bind.annotation.ExceptionHandler({ AmazonServiceException.class })
-	public ResponseEntity<Object> amazonServiceException(AmazonServiceException e, HttpServletRequest request) {
+	public ResponseEntity<Object> amazonServiceException(AmazonServiceException e, HttpServletRequest request, WebRequest res) {
 
 		HttpStatus statusCode = HttpStatus.valueOf(e.getErrorCode());
 		
@@ -96,7 +99,7 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	// Trata exceções AMAZON - 2.
 	// --------------------------------
 	@org.springframework.web.bind.annotation.ExceptionHandler({ AmazonClientException.class })
-	public ResponseEntity<Object> amazonClientException(AmazonClientException e, HttpServletRequest request) {
+	public ResponseEntity<Object> amazonClientException(AmazonClientException e, HttpServletRequest request, WebRequest res) {
 
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro para Cliente Service", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
@@ -105,7 +108,7 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	// Trata exceções AMAZON - 3.
 	// --------------------------------
 	@org.springframework.web.bind.annotation.ExceptionHandler({ AmazonS3Exception.class })
-	public ResponseEntity<Object> amazonS3Exception(AmazonS3Exception e, HttpServletRequest request) {
+	public ResponseEntity<Object> amazonS3Exception(AmazonS3Exception e, HttpServletRequest request, WebRequest res) {
 
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro para S3 Service", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
